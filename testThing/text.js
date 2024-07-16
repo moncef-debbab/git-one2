23a04b05ef19b076035aac8309345386ce7e7cdf
iam <moncefdebbab@gmail.com>
2024-07-16 12:41:00 +0100

    remove some files

--- testThing/text.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".off").onclick = function () {
-    document.querySelector("p").style.display = "none";
-};
-document.querySelector(".on").onclick = function () {
-    document.querySelector("p").style.display = "flex";
-};
